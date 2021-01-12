69ae6afb47ba5c891bbae9308fbd5de4ee9ce4c6
Kovarthanan07 <kovarthanan07@gmail.com>
2021-01-13 01:22:59 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,80 +1,14 @@
 const express = require("express");
 const app = express();
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post("/users", (req, res) => {
-  const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-});
-
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-});
-
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((error) => {});
-});
-
-app.get("/users/:id", (req, res) => {
-  const _id = req.params.id;
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.status(200).send(user);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
-});
-
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((error) => {});
-});
-
-app.get("/tasks/:id", (req, res) => {
-  const _id = req.params.id;
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.status(200).send(task);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("Server successfully running on :", port);
