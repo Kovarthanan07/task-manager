6c676f4eb1b530cfca1d14c4360753590b8c0287
Kovarthanan07 <kovarthanan07@gmail.com>
2021-01-21 23:40:42 +0530

    fourth commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const port = process.env.PORT || 3000;
+// app.use((req, res, next) => {
+//   if (req.method) {
+//     return res.status(503).send("Under Maintainance ");
+//   }
+//   next();
+// });
 
 app.use(express.json());
 app.use(userRouter);
@@ -13,3 +19,16 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server successfully running on :", port);
 });
+
+const jwt = require("jsonwebtoken");
+
+// const myFunction = () => {
+//   const token = jwt.sign({ _id: "184081D" }, "secretCode", {
+//     expiresIn: "30min",
+//   });
+//   console.log(token);
+
+//   const data = jwt.verify(token, "secretCode");
+//   console.log(data);
+// };
+// myFunction();
